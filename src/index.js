2e3218f6f6f46fed22c756f7d81e093655c0908a
Rebekah DeJong <rebekahdejong@Rebekahs-MacBook-Air.local>
2020-05-28 10:40:30 -0400

    added state to the app in order to continue making a class-based component

--- src/index.js
+++ src/index.js
@@ -11,35 +11,39 @@ import Pam from './images/PamBeesly.png';
 import Jim from './images/JimHalpert.png';
 import Oscar from './images/OscarMartinez.jpg';
 import Angela from './images/AngelaMartin.jpg';
-import quotes from './quotes.js';
-
-
-
-
+// import quotes from './quotes.js';
 
 
 class App extends React.Component {
-  render () {
-    const characters = [
+  constructor(props) {
+    super(props);
+
+    this.state = { char: [
       { name: "Michael Scott", title: "Regional Manager", image: Michael },
       { name: "Dwight Schrute", title: "Assistant Regional Manager", image: Dwight },
       { name: "Pam Beesly", title: "Secretary", image: Pam },
       { name: "Jim Halpert", title: "Salesman", image: Jim },
       { name: "Oscar Martinez", title: "Accountant", image: Oscar },
       { name: "Angela Martin", title: "Accountant", image: Angela }
-    ];
+    ] };
+
+  }
+  
+  render () {
+    console.log(this.state)
 
     return (
+      
       <div>
-        const makeCharacter = characters.map(character => {
+        {/* const makeCharacter = characters.map(character => { */}
           <RateButtons>
             <CharacterCard
-              name={ character.name }
-              title={ character.title }
-              image={ character.image }
+              name={ this.state.char[0].name }
+              title={ this.state.char[0].title }
+              image={ this.state.char[0].image }
             />
           </RateButtons>
-        });
+        {/* // }); */}
       </div>
     )
   }
